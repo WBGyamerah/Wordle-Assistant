a5444d8d06697723cc5d9c69ebe52c6c215c496d
Stealthender <william.gyamerah@outlook.com>
2025-06-30 21:15:35 +0100

    finished

--- Assistant/Library.cs
+++ Assistant/Library.cs
@@ -3,14 +3,50 @@ public class LibraryClass
     public static Dictionary<string, int> library()
     {
         Dictionary<string, int> wordScores = new Dictionary<string, int> { };
-        using StreamReader reader = new("C:/Users/zerte/OneDrive/Desktop/LetterBoxed Solver/wordlist.txt");
+        using StreamReader reader = new("C:/Users/zerte/OneDrive/Documents/GitHub/Wordle-Assistant/wordlist/wordlelist.txt");
         while (!reader.EndOfStream)
         {
             string word = reader.ReadLine()!; //Discovered an exclamation mark removes the warning
             int score = int.Parse(reader.ReadLine()!);
             wordScores.Add(word, score);
         }
-        var sorted = wordScores.OrderByDescending(pair => pair.Key.Distinct().Count()).ThenByDescending(pair => pair.Value); //Sort by unique characters, then sort by scores
+        var sorted = wordScores.OrderByDescending(pair => pair.Key.Distinct().Count()).ThenByDescending(pair => pair.Value) //Sort by unique characters, then sort by scores
+        .ToDictionary(pair => pair.Key, pair => pair.Value); //returns a list so needs to be turned back to a dictionary
+        return sorted;
+    }
+
+    public static Dictionary<string, int> removeLetter(Dictionary<string, int> wordScores, char letter, int pos, char marker)
+    {
+        switch (marker)
+        {
+            case 'G':
+                foreach (var wordScore in wordScores)
+                {
+                    if (wordScore.Key[pos] != letter)
+                    {
+                        wordScores.Remove(wordScore.Key);
+                    }
+                }
+                return wordScores;
+            case 'Y':
+                foreach (var wordScore in wordScores)
+                {
+                    if (wordScore.Key.Contains(letter) && wordScore.Key[pos] == letter)
+                    {
+                        wordScores.Remove(wordScore.Key);
+                    }
+                }
+                return wordScores;
+            case 'O':
+                foreach (var wordScore in wordScores)
+                {
+                    if (wordScore.Key.Contains(letter))
+                    {
+                        wordScores.Remove(wordScore.Key);
+                    }
+                }
+                return wordScores;
+        }
         return wordScores;
     }
 }
--- Assistant/Program.cs
+++ Assistant/Program.cs
@@ -1,13 +1,106 @@
-﻿public class MainClass
+﻿using System.Runtime.InteropServices;
+
+public class MainClass
 {
+    private static int wordLen = 5;
     public static void Main()
     {
-        Dictionary<string, int> wordScores = LibraryClass.library();
-        Console.WriteLine("Starting word suggestions: ");
-        for (int x = 0; x < 10; x++)
-        {
-            Console.WriteLine();
-        }
+        bool isfinished = false;
+        string word;
 
+        Dictionary<string, int> wordScores = LibraryClass.library();
+        while (isfinished == false)
+        {
+            suggest(wordScores);
+            word = getInput();
+            wordScores = checkColour(wordScores, word);
+            Console.WriteLine("Finished (Y/N)");
+            string answer = Console.ReadLine()!.ToUpper();
+            while (!answer.Equals("Y") && !answer.Equals("N"))
+            {
+                answer = Console.ReadLine()!.ToUpper();
+            }
+            if (answer.Equals("Y"))
+            {
+                isfinished = true;
+            }
+        }
     }
+    public static Dictionary<string, int> checkColour(Dictionary<string, int> wordScores,string word)
+    {
+        foreach (char letter in word)
+        {
+            int pos = word.IndexOf(letter); 
+            Console.WriteLine("What colour was " + letter + "(Green/Yellow/Grey)");
+            string answer = Console.ReadLine()!.ToUpper();
+            while (!answer.Equals("GREEN") && !answer.Equals("YELLOW") && !answer.Equals("GREY"))
+            {
+                answer = Console.ReadLine()!.ToUpper();
+            }
+            switch (answer)
+            {
+                case "GREEN":
+                    wordScores = LibraryClass.removeLetter(wordScores, letter, pos, 'G');
+                    break;
+                case "YELLOW":
+                    wordScores = LibraryClass.removeLetter(wordScores, letter, pos, 'Y');
+                    break;
+                case "GREY":
+                    wordScores = LibraryClass.removeLetter(wordScores, letter, pos, 'O');
+                    break;
+            }
+        }
+        return wordScores;
+    }
+    public static void suggest(Dictionary<string, int> wordScores)
+    {
+        Console.WriteLine("Suggestions: ");
+        foreach (var pair in wordScores.Take(10))
+        {
+            Console.WriteLine(pair.Key);
+        }
+    }
+
+    public static string getInput()
+    {
+        Console.WriteLine("What word was entered");
+        string input = Console.ReadLine()!;
+        while (validateInput(input) == false)
+        {
+            return getInput();
+        }
+        return input;
+    }
+
+    public static bool validateInput(string input) 
+    {
+        if (input.Length != wordLen)
+        {
+            Console.WriteLine("Invalid input, enter a word with" + wordLen + " letters");
+            return false;
+        }
+        for(int i = 0; i < input.Length; i++)
+        {
+            if (input[i] < 'a' || input[i] > 'z') //Checks if inputted elements are between a and z (Char.IsLetter allows letters not within the english alphabet)
+            {
+                Console.WriteLine("Invalid input, no symbols or numbers");
+                return false;
+            }
+        }
+        Console.WriteLine(input);
+        Console.WriteLine("Are these the correct sides Y/N");
+        string answer = Console.ReadLine()!.ToUpper();
+        while (!answer.Equals("Y") && !answer.Equals("N"))
+        {
+            answer = Console.ReadLine()!.ToUpper();
+        }
+        if (answer.Equals("N"))
+        {
+            return false;
+        }
+        return true;
+    }
+
+    
+
 }
